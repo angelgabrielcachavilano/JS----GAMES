1e37f96ac0919f5a1a2644eff21cf412f43281b9
angel gabriel cachavilano <angelcachavilano@outlook.com>
2024-04-30 00:00:01 -0300

    first commit

---
+++ arkanoid/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("init");
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d'); // 2D context rendering
+
+});
